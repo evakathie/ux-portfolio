d10d5099827378a0f21edbf6c4a9472d97de30af
Eva Katharina <evakathie@gmail.com>
2023-12-21 11:43:54 +0100

    Fixed Age in About page header

--- .vuepress/components/AboutHeader.vue
+++ .vuepress/components/AboutHeader.vue
@@ -7,7 +7,7 @@
           <p>Iʼm Eva, a {{ this.age }} year old Product Designer from Germany. Iʼm a creative outside-the-box thinker and passionate for Design Thinking. Living in the beautiful city of Copenhagen, I’m currently completing my Masterʼs degree in Information Studies at the <router-link to="/pages/company/aalborg-university.html">University of Aalborg</router-link>. Between my masterʼs and bachelorʼs degrees, I gained professional experience as a Product Designer at a Fintech company for several years. I believe in lifelong learning and love to improve myself and to dive into new challenges and opportunities.</p>
           <p>Outside the office, I enjoy spending time in nature or at the gym. I am an environmentalist, enjoy meeting friends and learning Danish. Creativity is also a big part of my free time, I have been an <router-link to="/pages/play.html">amateur photographer</router-link> for many years and enjoy pottery and sewing.</p>
       </div>
-      <img :src="'/images/about/eva-katharina-wolf-02.jpg'" alt="Professional job application photo: Eva stands in front of an office building in a blazer and white turtleneck shirt and smiles at the camera.">
+      <img :src="'/images/about/eva-katharina-wolf-02.jpg'" alt="Professional job application photo: Eva stands in front of an office building in a blazer and white turtleneck shirt and smiles.">
   </div>
 </template>
 
@@ -18,25 +18,16 @@ export default {
     computed: {
 
         age() {
-            const current_date = new Date();
-            let age = 0;
+            const birthDate = new Date('1996-07-22');
+            const currentDate = new Date();
 
-            let current_day = current_date.getDate();
-            let current_month = current_date.getMonth() + 1;
-            let current_year = current_date.getFullYear();
+            let age = currentDate.getFullYear() - birthDate.getFullYear();
 
-            let birth_day = 22;
-            let birth_month = 7;
-            let birth_year = 1996;
-
-            if (current_month >= birth_month) {
-                if (current_day >= birth_day) {
-                    age = current_year - birth_year;
-                } else {
-                    age = current_year - birth_year - 1;
-                }
-            } else {
-                age = current_year - birth_year - 1;
+            // Check if birthday has occurred this year
+            if (currentDate.getMonth() < birthDate.getMonth() ||
+                (currentDate.getMonth() === birthDate.getMonth() && currentDate.getDate() < birthDate.getDate())
+            ) {
+                age--;
             }
 
             return age;
